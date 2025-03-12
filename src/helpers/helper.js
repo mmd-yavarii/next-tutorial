4d351304d276ae1b70d53183f3589ba9f23a204a
mmd-yavarii <mdyavarii@gmail.com>
2025-03-12 15:02:40 +0330

    handle query strings

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -2,4 +2,41 @@ function shorternText(text) {
     return text.split(" ").slice(0, 3).join(" ");
 }
 
-export { shorternText };
+function searchProducts(products, search) {
+    if (!search) {
+        return products;
+    }
+    const result = products.filter((i) => i.title.toLowerCase().includes(search));
+    return result;
+}
+
+function filterProducts(products, category) {
+    if (!category) return products;
+    const result = products.filter((i) => i.category == category);
+    return result;
+}
+
+function createQueryObject(currentQuery, newQuery) {
+    if (newQuery.category == "all") {
+        const { category, ...rest } = currentQuery;
+        return rest;
+    }
+    if (newQuery.search == "") {
+        const { search, ...rest } = currentQuery;
+        return rest;
+    }
+    return { ...currentQuery, ...newQuery };
+}
+
+function getInitialQuery(searchParams) {
+    const query = {};
+    const category = searchParams.get("category");
+    const search = searchParams.get("search");
+
+    if (category) query.category = category;
+    if (search) query.search = search;
+
+    return query;
+}
+
+export { shorternText, searchProducts, filterProducts, createQueryObject, getInitialQuery };
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,24 +1,54 @@
+import { useEffect, useState } from "react";
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductProvider";
 
 import styles from "./ProductsPage.module.css";
 
+import { filterProducts, getInitialQuery, searchProducts } from "../helpers/helper";
+import { useSearchParams } from "react-router-dom";
+import SearchBox from "../components/SearchBox";
+import SideBar from "../components/SideBar";
+
 function ProductsPage() {
     const products = useProducts();
+    const [displayed, setDisplayed] = useState([]);
+
+    const [search, setSearch] = useState("");
+    const [query, setQuery] = useState({});
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    // link display with products
+    useEffect(() => {
+        setSearch(query.search || "");
+        setDisplayed(products);
+        setQuery(getInitialQuery(searchParams));
+    }, [products]);
+
+    useEffect(() => {
+        let finalProducts = searchProducts(products, query.search);
+        finalProducts = filterProducts(finalProducts, query.category);
+        setDisplayed(finalProducts);
+        setSearchParams(query);
+    }, [query]);
 
     return (
-        <div className={styles.container}>
-            <div className={styles.products}>
-                {!products.length && <Loader />}
+        <>
+            <SearchBox search={search} setSearch={setSearch} setQuery={setQuery} />
 
-                {products.map((product) => (
-                    <Card key={product.id} data={product} />
-                ))}
+            <div className={styles.container}>
+                <div className={styles.products}>
+                    {!products.length && <Loader />}
+
+                    {displayed.map((product) => (
+                        <Card key={product.id} data={product} />
+                    ))}
+                </div>
+
+                <SideBar setQuery={setQuery} query={query} />
             </div>
-
-            <div>side bar</div>
-        </div>
+        </>
     );
 }
 
